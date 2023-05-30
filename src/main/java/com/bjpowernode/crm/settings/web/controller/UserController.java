7fb5dda8831d8b59a18cb61123ea54ff6426dda4
yicongm2 <moyicong1999@163.com>
2023-05-30 01:21:01 -0400

    Cookie login

--- src/main/java/com/bjpowernode/crm/settings/web/controller/UserController.java
+++ src/main/java/com/bjpowernode/crm/settings/web/controller/UserController.java
@@ -10,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -27,7 +30,7 @@ public class UserController {
     }
 
     @RequestMapping("/settings/qx/user/login.do")
-    public @ResponseBody Object login(String loginAct, String loginPwd, String isRemPwd, HttpServletRequest request){
+    public @ResponseBody Object login(String loginAct, String loginPwd, String isRemPwd, HttpServletRequest request, HttpServletResponse response, HttpSession session){
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("loginAct", loginAct);
         map.put("loginPwd", loginPwd);
@@ -39,7 +42,7 @@ public class UserController {
         }else{
             String nowStr = DateUtils.formateDateTime(new Date());
 
-            if(nowStr.compareTo(user.getExpireTime()) > 0){
+            if(nowStr.compareTo(user.getExpireTime()) < 0){
 //                account is expired.
                 returnObject.setCode(Contants.RETURN_OBJECT_CODE_FAIL);
                 returnObject.setMessage("Account is expired");
@@ -55,9 +58,41 @@ public class UserController {
             }else{
 //                login successfully
                 returnObject.setCode(Contants.RETURN_OBJECT_CODE_SUCCESS);
+                session.setAttribute(Contants.SESSION_USER, user);
+                System.out.println(isRemPwd);
+                if("true".equals(isRemPwd)){
+                    Cookie c1 = new Cookie("loginAct", user.getLoginAct());
+                    c1.setMaxAge(10*24*60*60);
+                    response.addCookie(c1);
+                    Cookie c2 = new Cookie("loginPwd", user.getLoginPwd());
+                    c2.setMaxAge(10*24*60*60);
+                    response.addCookie(c2);
+                }else{
+                    Cookie c1 = new Cookie("loginAct", "");
+                    c1.setMaxAge(0);
+                    response.addCookie(c1);
+                    Cookie c2 = new Cookie("loginPwd", "");
+                    c2.setMaxAge(0);
+                    response.addCookie(c2);
+                }
             }
         }
 
         return returnObject;
+    }
+
+    @RequestMapping("/settings/qx/user/logout.do")
+    public String logout(HttpServletResponse response, HttpSession session){
+        Cookie c1 = new Cookie("loginAct", "");
+        c1.setMaxAge(0);
+        response.addCookie(c1);
+        Cookie c2 = new Cookie("loginPwd", "");
+        c2.setMaxAge(0);
+        response.addCookie(c2);
+
+        session.invalidate();
+
+        return "redirect:/";
+
     }
 }
--- src/main/webapp/WEB-INF/pages/settings/qx/user/login.jsp
+++ src/main/webapp/WEB-INF/pages/settings/qx/user/login.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%
 	String basePath=request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath() + "/";
 %>
@@ -11,18 +12,19 @@
 <script type="text/javascript" src="jquery/bootstrap_3.3.0/js/bootstrap.min.js"></script>
 <script type="text/javascript">
 	$(function (){
+		$(window).keydown(function(e){
+			if(e.keyCode == 13){
+				$("#loginBtn").click();
+			}
+
+		})
 		$("#loginBtn").click(function (){
 			var loginAct = $.trim($("#loginAct").val());
 			var loginPwd = $.trim($("#loginPwd").val());
 			var isRemPwd = $("#loginPwd").prop("checked");
-			if(loginAct == ""){
-				alert("Username is empty");
-				return;
-			}
-			if(loginPwd == ""){
-				alert("Password is empty");
-				return;
-			}
+
+			// $("#msg").html("正在登陆验证，请稍后");
+
 			$.ajax({
 				url: 'settings/qx/user/login.do',
 				data: {
@@ -39,6 +41,20 @@
 					}else{
 						$("#msg").html(data.message);
 					}
+				},
+				beforeSend: function(){
+					if(loginAct == ""){
+						alert("Username is empty");
+						return false;
+					}
+					if(loginPwd == ""){
+						alert("Password is empty");
+						return false;
+					}
+
+					$("#msg").html("正在登陆验证，请稍后");
+					return true;
+
 				}
 			})
 		});
@@ -61,14 +77,21 @@
 			<form action="workbench/index.html" class="form-horizontal" role="form">
 				<div class="form-group form-group-lg">
 					<div style="width: 350px;">
-						<input class="form-control" id="loginAct" type="text" placeholder="用户名">
+						<input class="form-control" id="loginAct" type="text" value="${cookie.loginAct.value}" placeholder="用户名">
 					</div>
 					<div style="width: 350px; position: relative;top: 20px;">
-						<input class="form-control" id="loginPwd" type="password" placeholder="密码">
+						<input class="form-control" id="loginPwd" type="password" value="${cookie.loginPwd.value}" placeholder="密码">
 					</div>
 					<div class="checkbox"  style="position: relative;top: 30px; left: 10px;">
 						<label>
-							<input type="checkbox" id="isRemPwd"> 十天内免登录
+							<c:if test="${not empty cookie.loginAct and not empty cookie.loginPwd}">
+								<input type="checkbox" id="isRemPwd" checked>
+							</c:if>
+							<c:if test="${empty cookie.loginAct or empty cookie.loginPwd}">
+								<input type="checkbox" id="isRemPwd">
+							</c:if>
+<%--							<input type="checkbox" id="isRemPwd"> --%>
+							十天内免登录
 						</label>
 						&nbsp;&nbsp;
 						<span id="msg"></span>
